d125bcb4365c13f858441bbfb012755e3b882fee
mi0706chi <mi0706chi@gmail.com>
2021-01-10 13:36:26 +0900

    Add rails tutorial codes, Change basical design and Add create_micropost_path

---
+++ tutorial/config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
+  get 'sessions/new'
+  get 'session/new'
+  get 'foo/bar'
+  get 'foo/baz'
+  root'static_pages#home'
+  get '/help' ,to: 'static_pages#help'
+  get '/about' ,to: 'static_pages#about'
+  get '/contact' ,to: 'static_pages#contact'
+  resources :microposts,          only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships,       only: [:create, :destroy]
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup' ,to: 'users#new'
+  get '/create_micropost' ,to: 'static_pages#create_micropost'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+end
