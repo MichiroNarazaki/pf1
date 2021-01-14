8058959d951ca642e8fe9a8fb79f6427c02146ef
lowzzy <mi0706chi@gmail.com>
2021-01-14 21:45:15 +0900

    Add simple test

--- tutorial/test/controllers/likes_controller_test.rb
+++ tutorial/test/controllers/likes_controller_test.rb
@@ -1,7 +1,40 @@
-require 'test_helper'
+require "test_helper"
 
 class LikesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+    @my_micropost = Micropost.find_by(user_id: @user.id)
+    @other_micropost = Micropost.find_by(user_id: @other_user.id)
+  end
+
+  test "should create like my micropost" do
+    log_in_as(@user, remember_me: "1")
+    get root_path
+    assert_difference "Like.count", 1 do
+      post micropost_likes_path(@my_micropost)
+    end
+  end
+  
+  test "should create like other's micropost" do
+    log_in_as(@user, remember_me: "1")
+    get root_path
+    assert_difference "Like.count", 1 do
+      post micropost_likes_path(@other_micropost)
+    end
+  end
+  test "should like just only once" do
+    log_in_as(@user, remember_me: "1")
+    assert_difference "Like.count", 1 do
+      post micropost_likes_path(@my_micropost)
+      post micropost_likes_path(@my_micropost)
+    end
+  end
+  test "should destroy like" do
+    log_in_as(@user, remember_me: "1")
+    assert_no_difference "Like.count" do
+      post micropost_likes_path(@my_micropost)
+      delete micropost_like_path(@my_micropost,id: @user.id)
+    end
+  end
 end
