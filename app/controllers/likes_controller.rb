0217c2fd15070ef8d9be768f7778b6c92fc40d71
lowzzy <mi0706chi@gmail.com>
2021-01-29 02:05:33 +0900

    Change how to be banned to using before_action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
-    def create
-        if current_user.guest
-            flash[:danger] = "いいねするには登録してください"
-        else
-            @like = current_user.likes.create(micropost_id: params[:micropost_id])
-        end
-        redirect_back(fallback_location: root_path)
-    end
-    def destroy
-        @like = Like.find_by(micropost_id: params[:micropost_id],user_id: current_user.id)
-        @like.destroy
-        redirect_back(fallback_location: root_path)
-    end
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @like = current_user.likes.create(micropost_id: params[:micropost_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @like = Like.find_by(micropost_id: params[:micropost_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
