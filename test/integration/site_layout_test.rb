29b23eb7fe0474e4fc504dce4ba5c679edfd66e8
lowzzy <mi0706chi@gmail.com>
2021-02-07 22:27:12 +0900

    Fix layout test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,10 +9,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   log_in_as(@user, remember_me: '1')
   get root_path
   assert_template 'static_pages/home'
-  assert_select "a[href=?]", logout_path, count: 1#headerに1つ
-  # assert_select "a[href=?]", root_path, count: 2#headerに1つ,footerに1つ(1番左) //rankingができたらコメントアウト外す
-  # assert_select "a[href=?]", ranking_path, count: 1#footerに1つ(左から2番目) //rankingができたらコメントアウト外す
+  assert_select "a[href=?]", create_micropost_path, count: 1#headerに1つ
+  assert_select "a[href=?]", search_path, count: 1#headerに1つ,footerに1つ(1番左) //rankingができたらコメントアウト外す
+  assert_select "a[href=?]", ranking_path, count: 1#footerに1つ(左から2番目)
   assert_select "a[href=?]", create_micropost_path, count:1
+  assert_select "a[href=?]", root_path, count:2
   assert_select "footer a[href=?]", user_path(@user), count:1
   end
 end
