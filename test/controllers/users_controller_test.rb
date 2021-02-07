116df7ff44c6cd7714c0774a75ebbab5a5458e65
lowzzy <mi0706chi@gmail.com>
2021-02-08 01:21:01 +0900

    Updated by test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,7 +13,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect index when not logged in" do
     get users_path
-    assert_redirected_to login_url
+    assert_redirected_to signup_url
   end
   test "should show user" do
     log_in_as(@user)
@@ -29,14 +29,14 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
     assert_not flash.empty?
-    assert_redirected_to login_url
+    assert_redirected_to signup_url
   end
 
   test "should redirect update when not logged in" do
     patch user_path(@user), params: { user: { name: @user.name,
                                              email: @user.email } }
     assert_not flash.empty?
-    assert_redirected_to login_url
+    assert_redirected_to signup_url
   end
   test "should redirect edit when logged in as wrong user" do
     log_in_as(@other_user)
@@ -67,7 +67,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference "User.count" do
       delete user_path(@user)
     end
-    assert_redirected_to login_url
+    assert_redirected_to signup_url
   end
 
   test "should redirect destroy when logged in as a non-admin" do
