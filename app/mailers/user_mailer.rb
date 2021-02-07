464d10c054bfb75e364918025c481337cce3d111
lowzzy <mi0706chi@gmail.com>
2021-02-08 01:20:38 +0900

    Translate into Japanese

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "アカウント有効化についてのご連絡"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "パスワードリセットについてのご連絡"
   end
 end
