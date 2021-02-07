b365adeaf2009cd8cedb7f0d075b188877fdd690
lowzzy <mi0706chi@gmail.com>
2021-02-08 00:10:44 +0900

    Fix website title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-		@base_title = "Ruby on Rails Tutorial Sample App"
+		@base_title = "YouIdea"
     @user = users(:michael)
   end
 
@@ -11,23 +11,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_path
     assert_response :success
 	  assert_select "title","#{@base_title}"
-end
-
-test "should get help" do
-    get help_path
-    assert_response :success
-		assert_select "title","Help | #{@base_title}"
-end
-
-test "should get about" do
-    get about_path
-    assert_response :success
-		assert_select "title","About | #{@base_title}"
-  end
-
-test "should get contact" do
-    get contact_path
-    assert_response :success
-		assert_select "title","Contact | #{@base_title}"
   end
 end
