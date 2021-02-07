b00c38373c8467a559bc0454e2e7211a377e9c83
lowzzy <mi0706chi@gmail.com>
2021-02-08 00:11:59 +0900

    Adapted to microposts needs title

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -62,7 +62,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "associated microposts should be destroyed" do
     @user.save
-    @user.microposts.create!(content: "Lorem ipsum")
+    @user.microposts.create!(title: "Lorem ipsum",content: "Lorem ipsum")
     assert_difference 'Micropost.count', -1 do
       @user.destroy
     end
