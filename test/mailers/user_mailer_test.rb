33c465f08dce0b76fb442f6361fc15b576927edd
lowzzy <mi0706chi@gmail.com>
2021-02-10 17:03:29 +0900

    Translate into Ja

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-
+  
   test "account activation" do
     user = users(:michael)
     user.activation_token = User.new_token
@@ -9,9 +9,9 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "アカウント有効化についてのご連絡", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    assert_match user.name,               mail.html_part.body.encoded
+    assert_match user.activation_token,   mail.html_part.body.encoded
+    assert_match CGI.escape(user.email),  mail.html_part.body.encoded
   end
 
   test "password reset" do
@@ -21,7 +21,7 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal "パスワードリセットについてのご連絡", mail.subject
     assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match user.reset_token,        mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    assert_match user.reset_token,        mail.html_part.body.encoded
+    assert_match CGI.escape(user.email),  mail.html_part.body.encoded
   end
 end
